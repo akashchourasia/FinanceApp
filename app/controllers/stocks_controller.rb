ba0bc0a9e73dc27f5911e2e51e862b857ec4c659
akash's app <chourasiaakash05@gmail.com>
2018-07-19 23:55:43 +0530

    Completed This App

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,23 +1,14 @@
 class StocksController < ApplicationController
 
     def search
-        if params[:stock].present?
+        if params[:stock].blank?
+            flash.now[:danger] = "You have entered an empty search string"
+        else
             @stock = Stock.new_from_lookup(params[:stock])
-            if @stock
-                respond_to do |format|
-                    format.js { render partial: 'users/result' }
-                end
-            else
-                respond_to do |format|
-                    flash.now[:danger] = "You have entered an incorrect symbol"
-                    format.js { render partial: 'users/result' }
-                end
-            end
-            else
-                respond_to do |format|
-                    flash.now[:danger] = "You have entered an empty string"
-                    format.js { render partial: 'users/result' }
-                end
-            end
+            flash.now[:danger] = "You have entered an incorrect symbol" unless @stock
         end
+            respond_to do |format|
+                format.js { render partial: 'users/result' }
+            end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   get 'search_stocks', to: "stocks#search"
   resources :user_stocks, only: [:create, :destroy]
   get 'my_friends', to: "users#my_friends"
+  get 'search_friends', to: "users#search"
+  post 'add_friend', to: 'users#add_friend'
+  resources :users, only: [:show]
+  resources :friendships
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
